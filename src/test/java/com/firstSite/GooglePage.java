578bca8abf23065f7b9c08f094d0058b23de746e
igor5566 <volkoff5566@ukr.net>
2020-02-02 17:09:34 +0200

    From home

--- src/test/java/com/firstSite/GooglePage.java
+++ src/test/java/com/firstSite/GooglePage.java
@@ -12,7 +12,7 @@ import org.testng.annotations.Test;
 import static org.openqa.selenium.support.ui.ExpectedConditions.*;
 import static org.testng.Assert.assertTrue;
 
-public class GooglePage /*extends FirstClass */{
+public class GooglePage extends FirstClass {
 
     @Test()
     public void getSearchPage() {
@@ -20,22 +20,22 @@ public class GooglePage /*extends FirstClass */{
         WebDriver driver = new FirefoxDriver();
         driver.get("https://www.pdffiller.com/en/login.htm");
         driver.quit();
-//        WebElement login = wait.until(visibilityOfElementLocated(By.cssSelector("input[name='username']")));
-//        login.sendKeys("test555@gmail.com");
-//        WebElement pass = wait.until(visibilityOfElementLocated(By.cssSelector("input[name='password']")));
-//        pass.sendKeys(password, Keys.ENTER);
-//
-//        WebElement add = wait.until(elementToBeClickable(By.cssSelector("button[class='g-btn g-btn--primary g-btn--sm']")));
-//        actions.moveToElement(add).build().perform();
-//        WebElement addDocumentBtn = wait.until(visibilityOfElementLocated(By.cssSelector("ul[class='md-head-options'] > li:nth-child(1) > button")));
-//        addDocumentBtn.click();
-//
-//        WebElement input = wait.until(presenceOfElementLocated(By.cssSelector("div[class='drop-zone'] input[accept]")));
-//        input.sendKeys("/home/igor/doc1");
-//
-//        wait.until(presenceOfElementLocated(By.cssSelector("div[id='DoneButton'] > button")));
-//        String s = driver.getCurrentUrl();
-//        assertTrue(s.contains("jsfiller"));
+        WebElement login = wait.until(visibilityOfElementLocated(By.cssSelector("input[name='username']")));
+        login.sendKeys("test555@gmail.com");
+        WebElement pass = wait.until(visibilityOfElementLocated(By.cssSelector("input[name='password']")));
+        pass.sendKeys("test12345", Keys.ENTER);
+
+        WebElement add = wait.until(elementToBeClickable(By.cssSelector("button[class='g-btn g-btn--primary g-btn--sm']")));
+        actions.moveToElement(add).build().perform();
+        WebElement addDocumentBtn = wait.until(visibilityOfElementLocated(By.cssSelector("ul[class='md-head-options'] > li:nth-child(1) > button")));
+        addDocumentBtn.click();
+
+        WebElement input = wait.until(presenceOfElementLocated(By.cssSelector("div[class='drop-zone'] input[accept]")));
+        input.sendKeys("/home/igor/doc1");
+
+        wait.until(presenceOfElementLocated(By.cssSelector("div[id='DoneButton'] > button")));
+        String s = driver.getCurrentUrl();
+        assertTrue(s.contains("jsfiller"));
     }
 
 
